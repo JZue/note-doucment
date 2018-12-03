6248445198530919af2478718ada6f6275e39a21
JZue <646980564@qq.com>
2018-12-03 18:25:18 +0800

    netty

--- study-src/src/main/java/com/jzue/study/netty/netty2_1/Server.java
+++ study-src/src/main/java/com/jzue/study/netty/netty2_1/Server.java
@@ -4,6 +4,7 @@ package com.jzue.study.netty.netty2_1;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -21,7 +22,7 @@ public class Server {
        */
       public Server( int port){
             try {
-                  this.serverSocket=new ServerSocket();
+                  this.serverSocket=new ServerSocket(port);
                   log.info("服务端启动成功，端口：{}",port);
             }catch (IOException e) {
                   e.printStackTrace();
@@ -34,13 +35,25 @@ public class Server {
                   public void run() {
                         listen();
                   }
-            });
+            }).start();
       }
+
       private void listen(){
             while (true){
                   try {
                         //监听端口
                         Socket client =serverSocket.accept();
+                        InputStream inputStream =client.getInputStream();
+                        while (true){
+                              byte[] data =new  byte[1024];
+                              int len;
+                              while ((len=inputStream.read(data))!=-1){
+                                    String ms=new String(data,0,len);
+                                    System.out.println("客户端传来的消息："+ms);
+                              }
+                        }
+
+
                   }catch (IOException e){
                         e.printStackTrace();
                   }
