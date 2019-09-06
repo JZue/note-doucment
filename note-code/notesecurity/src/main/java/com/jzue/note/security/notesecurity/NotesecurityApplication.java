edb0aa1585924b034c84bec5453137cb069ba295
JZue <646980564@qq.com>
2019-09-06 08:14:21 +0800

    security

---
+++ note-code/notesecurity/src/main/java/com/jzue/note/security/notesecurity/NotesecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jzue.note.security.notesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesecurityApplication.class, args);
+    }
+
+}
---
+++ note-code/notesecurity/src/test/java/com/jzue/note/security/notesecurity/NotesecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jzue.note.security.notesecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotesecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
