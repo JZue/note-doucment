3842a40d589f6d261a9a789b1da929a915b080cf
JZue <646980564@qq.com>
2019-09-08 14:18:34 +0800

    effective java 学习笔记

---
+++ note-code/effectivejava/src/main/java/com/jzue/note/effectivejava/EffectivejavaApplication.java
@@ -0,0 +1,13 @@
+package com.jzue.note.effectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectivejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectivejavaApplication.class, args);
+    }
+
+}
---
+++ note-code/effectivejava/src/main/java/com/jzue/note/effectivejava/chapter2/Item1.java
@@ -0,0 +1,11 @@
+package com.jzue.note.effectivejava.chapter2;
+
+import java.io.Serializable;
+
+/**
+ * 参考  https://www.cnblogs.com/honoka/p/4858416.html，其中有很多详细例子，理解一下即可
+ * @author jzue
+ * @date 2019/9/8 上午10:18
+ **/
+public class Item1 {
+}
