ccfebf6b5d71507aaf82cd521c70499719372867
JZue <646980564@qq.com>
2020-08-03 12:34:01 +0800

    ϵͳ¶¨ʱÈÎÎñ×Զ¯Ìύ

--- note-commit.sh
+++ note-commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+cd /Users/jzue/Desktop/git-repo/note-doucment
 unset msg
 
-read -p "请输入commit提交的描述: " msg
-
+#read -p "请输入commit提交的描述: " msg
+msg="系统定时任务自动提交"
 git add -A
 git commit -m $msg
 git push
