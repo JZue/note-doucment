0313cc2acdd2a889bbc505e4a1fe1fa30d8f79d2
JZue <646980564@qq.com>
2020-08-03 12:43:41 +0800

    crontab task auto commit...

--- note-commit.sh
+++ note-commit.sh
@@ -3,5 +3,4 @@ cd /Users/jzue/Desktop/git-repo/note-doucment
 git add .
 git commit -m 'crontab task auto commit...'
 git push origin master
-git status
-
+echo "------------------------------------------------------------------------------------"
