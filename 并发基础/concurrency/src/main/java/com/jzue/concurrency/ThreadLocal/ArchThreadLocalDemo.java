83a535e1d82c89fd3206f438d30f8965dc76cd00
JZue <646980564@qq.com>
2021-10-14 16:42:41 +0800

    crontab task auto commit...

---
+++ 并发基础/concurrency/src/main/java/com/jzue/concurrency/ThreadLocal/ArchThreadLocalDemo.java
@@ -0,0 +1,10 @@
+package com.jzue.concurrency.ThreadLocal;
+
+/**
+ * @author xuejunze
+ * @date 2021/10/14 3:08 下午
+ **/
+public class ArchThreadLocalDemo {
+    static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+}
